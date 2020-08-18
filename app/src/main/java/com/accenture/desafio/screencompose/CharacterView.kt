3a2b9b47f342c979736f88cdcc4babefce5a063a
Alves <victor.de.s.alves@accenture.com>
2020-08-17 21:33:20 -0300

    incluindo jetpack compose

---
+++ app/src/main/java/com/accenture/desafio/screencompose/CharacterView.kt
@@ -0,0 +1,4 @@
+package com.accenture.desafio.screencompose
+
+class CharacterView {
+}
---
+++ app/src/main/java/com/accenture/desafio/screencompose/ui/Shape.kt
@@ -0,0 +1,12 @@
+package com.accenture.desafio.screencompose.ui
+
+import androidx.ui.foundation.shape.corner.RoundedCornerShape
+import androidx.ui.material.Shapes
+
+import androidx.ui.unit.dp
+
+val shapes = Shapes(
+        small = RoundedCornerShape(4.dp),
+        medium = RoundedCornerShape(4.dp),
+        large = RoundedCornerShape(0.dp)
+)
